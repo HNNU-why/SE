5f16a5dda82370b35e14ca6c6001a629e4551c9c
Admin <2648226350@qq.com>
2022-11-08 15:38:04 +0800

    摸鱼

---
+++ Craw/BiliBili/Spider.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import math
+
+
+class Spider:
+
+    def __init__(self, headers):
+        self.headers = headers
+
+    def getVideo(self, search, maxVideo):
+        videoList = []
+        print('正在获取视频列表.......')
+        for i in range(maxVideo):
+            video_url = 'https://api.bilibili.com/x/web-interface/search/all/v2?keyword={}&page={' \
+                        '}&duration=&tids_1=&tids_2=&__refresh__=true&_extra=&highlight=1&single_column=0&jsonp=jsonp' \
+                        ''.format(search, i + 1)
+            resp = requests.get(video_url, headers=self.headers)
+            data = json.loads(resp.text)
+            try:
+                video_data = data['data']['result'][10]['data']
+                for d in video_data:
+                    videoList.append(d['id'])
+            except KeyError:
+                print('KeyError')
+        print('视频列表获取成功！')
+        return videoList
+
+    # 获取评论页数
+    def getReplyPageNum(self, oid):
+        url = "https://api.bilibili.com/x/v2/reply?&jsonp=jsonp&pn=1" + "&type=1&oid=" + str(oid) + "&sort=2"
+        respond = requests.get(url)
+        res_dirct = json.loads(respond.text)
+        replyPageNum = 1
+        try:
+            replyNum = int(res_dirct['data']['page']['acount'])
+            replyPageCount = int(res_dirct['data']['page']['count'])
+            replyPageSize = int(res_dirct['data']['page']['size'])
+            replyPageNum = math.ceil(replyPageCount / replyPageSize)
+        except KeyError:
+            print("KeyError")
+        return replyPageNum
+
+    # Bv号转化为av号
+    # def getAid(bvid):
+    #     url = "http://api.bilibili.com/x/web-interface/view?bvid="+str(bvid)
+    #     response = requests.get(url)
+    #     dirt=json.loads(response.text)
+    #     aid=dirt['data']['aid']
+    #     return aid
+
+    def getComment(self, video, maxPage):
+        messageList = []
+        print("正在从视频", video, " 获取评论")
+        for page in range(1, self.getReplyPageNum(video) + 1):
+            url = 'https://api.bilibili.com/x/v2/reply/main?mode=3&next={}&oid={}&plat=1&type=1'.format(page, video)
+            resp = requests.get(url, headers=self.headers)
+            try:
+                replies = resp.json()['data']['replies']
+                for reply in replies:
+                    messageList.append(reply['content']['message'])
+            except KeyError:
+                print(KeyError)
+            except TypeError:
+                print(TypeError)
+            # 控制每个视频爬取的评论页数
+            if page > maxPage:
+                break
+        print("评论获取成功！")
+        return messageList
+
+    def saveComment(self, data, name):
+        print("正在保存评论。。。。")
+        with open('Data/{}.json'.format(name), 'a', encoding='utf-8') as fp:
+            json.dump(data, fp, ensure_ascii=False)
+            fp.close()
+        print("评论保存成功")
---
+++ djangoProject/search/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from Craw.BiliBili.Spider import Spider
+
+
+def craw(keyWord):
+    headers = {
+        'accept': 'application/json, text/plain, */*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42',
+        'cookie': "buvid3=341E375F-63BF-D0E6-2D66-4F81DFBD059357749infoc; b_nut=1667868357; i-wanna-go-back=-1; "
+                  "_uuid=7911D9C6-A1C5-2795-8E95-59E12555CDCF57926infoc; "
+                  "buvid4=D2EB1F5B-17FA-0CB8-8C66-C1F8DB326AF058385-022110808-OrIQ3bLaNqvw1dl9ZT6Nkg%3D%3D; "
+                  "nostalgia_conf=-1; rpdid=|(J|)RJYYRkl0J'uYY~)Y)J~m; hit-new-style-dyn=0; hit-dyn-v2=1; "
+                  "share_source_origin=COPY; fingerprint=9d95cddb8278d5b55fb43d6fed67a6b4; buvid_fp_plain=undefined; "
+                  "SESSDATA=9151d446%2C1683420470%2C92582%2Ab1; bili_jct=dc81f334ec7c4c553398fbe35f2d7933; "
+                  "DedeUserID=399834015; DedeUserID__ckMd5=6b331073964aa2e6; sid=5nkyr2i3; "
+                  "buvid_fp=9d95cddb8278d5b55fb43d6fed67a6b4; PVID=1; b_ut=5; bsource=search_bing; innersign=1; "
+                  "CURRENT_QUALITY=64; bp_video_offset_399834015=726024278369108100; b_lsid=827B4FFF_18455E82BB3; "
+                  "CURRENT_FNVAL=16",
+        'Connection': 'keep-alive',
+    }
+    messageList = []
+    spider = Spider(headers)
+    videoList = spider.getVideo(keyWord, 1)     # 取前1页个视频
+    for video in videoList:                     # 取每个视频前1页评论
+        messageList.append(spider.getComment(video, 10))
+    return messageList
+
+
+def search(request):
+    if request.method == 'POST':
+        keyword = request.POST.get('keyWord')
+    print(keyword)
+    messageList = craw(keyword)
+    return render(request, 'show.html', {"comment" : messageList})
+
+
+def index(request):
+    return render(request, 'search.html')
+
