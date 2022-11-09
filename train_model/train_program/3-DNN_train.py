6c409c2916093393bd855f310eb851932ce17777
HNNU-why <117609439+HNNU-why@users.noreply.github.com>
2022-11-09 15:48:53 +0800

    Update 3-DNN_train.py

--- train_model/train_program/3-DNN_train.py
+++ train_model/train_program/3-DNN_train.py
@@ -24,42 +24,28 @@ class Dataset(torch.utils.data.Dataset):
 dataset = Dataset('train')
 
 print(dataset[0])
-
-# 加载字典和分词工具
-token = BertTokenizer.from_pretrained('bert-base-chinese')  # 加载预分词器
+token = BertTokenizer.from_pretrained('bert-base-chinese') 
 print(token)
 
 
 def collate_fn(data):
     sents = [i[0] for i in data]
     labels = [i[1] for i in data]
-
-    # 编码---使用自带的批量成对编码
-    data = token.batch_encode_plus(batch_text_or_text_pairs=sents,  # 传递数据的格式有所变化
-                                   truncation=True,  # 达到最大长度后截断
-                                   # 达到最大长度后截断
+    data = token.batch_encode_plus(batch_text_or_text_pairs=sents,  
+                                   truncation=True, 
                                    padding='max_length',
                                    max_length=500,
-                                   # 返回pytorch类型，默认是返回list
                                    return_tensors='pt',
-                                   # 返回length，标识长度
                                    return_length=True)
-    # 将分词后的结果取出来
-    # input_ids:编码之后的数字
-    # attention_mask:是补零的位置是0,其他位置是1
     input_ids = data['input_ids']
     attention_mask = data['attention_mask']
     token_type_ids = data['token_type_ids']
     labels = torch.LongTensor(labels)
-
-    #print(data['length'], data['length'].max())
-
     return input_ids, attention_mask, token_type_ids, labels
-
 
 # 数据加载器
 loader = torch.utils.data.DataLoader(dataset=dataset,
-                                     batch_size=32,  # 决定与训练模型的单次训练句子，越大则计算出来的准确率就越平稳
+                                     batch_size=32, 
                                      collate_fn=collate_fn,
                                      shuffle=True,
                                      drop_last=True)
@@ -68,13 +54,10 @@ for i, (input_ids, attention_mask, token_type_ids,
         labels) in enumerate(loader):
     break
 
-print(len(loader))  # 批处理大小batch_size*loder的大小等于训练集train的长度
-
 # 加载预训练模型
 pretrained = BertModel.from_pretrained(
     'bert-base-chinese')  # 通过name加载网络上的与训练模型，并且缓存到cache中
 
-# 不训练,不需要计算梯度：不会修改预训练模型的参数，只训练下游任务模型
 for param in pretrained.parameters():
     param.requires_grad_(False)
 
@@ -82,13 +65,9 @@ for param in pretrained.parameters():
 out = pretrained(input_ids=input_ids,
                  attention_mask=attention_mask,
                  token_type_ids=token_type_ids)  # 将tokenizer分词器得到的结果参数直接喂给预训练模型
-# 输出最后一个隐藏层的参数类型 --torch.Size(16,500,768)
-# 16:batchSize,每一批训练16个句子，500是句子长度（多则截断，短则补长），768是词编码的维度，即每个词编码成768维度的向量
-print(out.last_hidden_state.shape)
 
 # 使用bert抽取特征，然后再做下游任务的迁移学习
 # 定义下游任务模型
-
 
 class Model(torch.nn.Module):
     def __init__(self):
@@ -107,7 +86,6 @@ class Model(torch.nn.Module):
             out = pretrained(input_ids=input_ids,
                              attention_mask=attention_mask,
                              token_type_ids=token_type_ids)
-        # 将预训练模型抽取数据中的特征（情感分类只取第0个词的特征）交给全连接层计算
         out = self.fc1(out.last_hidden_state[:, 0])
         out = self.relu1(out)
         out = self.fc2(out)
@@ -116,10 +94,8 @@ class Model(torch.nn.Module):
         out = out.softmax(dim=1)  # 指定第1个维度的和为1
         return out  # torch(32,1)
 
-
 model = Model()
 model.load_state_dict(torch.load('./model/3-DNN_model.pth'))
-
 
 # 训练
 optimizer = AdamW(model.parameters(), lr=1e-3)
@@ -127,18 +103,17 @@ criterion = torch.nn.CrossEntropyLoss()
 
 model.train()
 for i, (input_ids, attention_mask, token_type_ids,
-        labels) in enumerate(loader):  # loader一共有300层，将tokenizer的分词结果输出给预训练模型
+        labels) in enumerate(loader):  
     out = model(input_ids=input_ids,
                 attention_mask=attention_mask,
-                token_type_ids=token_type_ids)  # 得到前向传播的结果
+                token_type_ids=token_type_ids) 
 
-    loss = criterion(out, labels)  # 得到损失函数
-    loss.backward()  # 通过损失率反向传播
+    loss = criterion(out, labels) 
+    loss.backward() 
     optimizer.step()  # 优化
     optimizer.zero_grad()  # 归0梯度
 
     if i % 5 == 0:
-        # torch(32,2),返回指定维度最大值的序号  0 or 1 ，即最终分析的句子是正面的还是反面的
         out = out.argmax(dim=1)
         accuracy = (out == labels).sum().item() / len(labels)
         print("第" + str(i) + "轮  ",
@@ -153,7 +128,6 @@ for i, (input_ids, attention_mask, token_type_ids,
 
 torch.save(model.state_dict(), './model/3-DNN_model.pth')
 print("保存成功！")
-
 
 # modeltest = Model()
 # modeltest.load_state_dict(torch.load('./model/modelpara.pth'))
